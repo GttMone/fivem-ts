eadaa1abcba44697adf3338331f566b77d04adcc
Vito Gos <davidgospodinov10@gmail.com>
2024-06-27 04:35:22 +0300

    Updated fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,6 @@ description 'FiveM TypeScript Template'
 version '1.0.0'
 repo 'https://github.com/GttMone/fivem-ts'
 
-shared_script 'config.js'
+shared_script 'config/**'
 client_script 'dist/client/**'
 server_script 'dist/server/**'
